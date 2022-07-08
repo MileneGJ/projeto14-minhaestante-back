396829cecb409784e3ac4e5f27e41261afafe598
milenegj <milene.jannetti@gmail.com>
2022-07-08 07:37:37 -0500

    Creating db

---
+++ src/dbStrategy/mongodb.js
@@ -0,0 +1,14 @@
+import {MongoClient} from 'mongodb';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const client = new MongoClient(process.env.MONGO_URI);
+let db;
+client.connect(() => {
+    db = client.db(process.env.DATABASE);
+});
+
+const objectId = ObjectId;
+
+export { db, objectId };

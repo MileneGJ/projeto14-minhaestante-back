6f9156b929272e2f4c4de08b56e783822f2dc55a
milenegj <milene.jannetti@gmail.com>
2022-07-10 14:11:23 -0500

    Book schema fix type value

--- src/schemas/bookSchemas.js
+++ src/schemas/bookSchemas.js
@@ -5,7 +5,7 @@ export const newBookSchema = joi.object({
     description:joi.string().min(5).required(),
     author:joi.string().required(),
     price:joi.string().regex(/^R\$[0-9]+\,[0-9]{2}$/).required(),
-    type:joi.string().valid('ebook','livro físico').required(),
+    type:joi.string().valid('Ebook','Livro Físico').required(),
     genre:joi.string().required(),
     image:joi.string().uri(),
     publisher:joi.string(),
